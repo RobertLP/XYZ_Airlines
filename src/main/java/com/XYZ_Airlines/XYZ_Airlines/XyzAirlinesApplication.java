3912a76e4ab39d11d5ae1f3ecece05e13c694bc4
Robert Popijus <robert.popijus@capgemini.com>
2018-10-08 09:22:23 +0200

    Project setup

---
+++ src/main/java/com/XYZ_Airlines/XYZ_Airlines/XyzAirlinesApplication.java
@@ -0,0 +1,12 @@
+package com.XYZ_Airlines.XYZ_Airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzAirlinesApplication.class, args);
+	}
+}
